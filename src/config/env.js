ec6b595968c76776970367e8aaea3bdbebdddf6a
julioramcoder <julioramirez79@gmail.com>
2026-02-25 22:13:57 -0500

    feat: actualizar configuración de variables de entorno

--- src/config/env.js
+++ src/config/env.js
@@ -9,7 +9,7 @@ export const env = {
     DATABASE_URL: process.env.DATABASE_URL, // con esto conectamos la aplicacion con la base de datos 
     MONGODB_URI: process.env.MONGODB_URI, // direccion para conectarse a mongo 
     MONGODB_DB: process.env.MONGODB_DB, // define que base dentro de mongo usar 
-    SIMULACRO_CSV_PATH: process.env. SIMULACRO_CSV_PATH //guarda la ruta del archivo csv
+    SIMULACRO_CSV_PATH: process.env.SIMULACRO_CSV_PATH //guarda la ruta del archivo csv
 };
 
 // acontinuacion se hara una validacion de seguridad, recorre una lista de nombres de variables importantes y verifica la dxistencia de cada una
